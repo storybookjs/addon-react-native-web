e2e993e361446ae6238e8b9335b456b799f76033
Daniel Williams <danny.h.williams@hotmail.com>
2021-10-15 18:18:33 +0100

    feat: use babelv7 mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,4 +4,11 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['../preset.js', '@storybook/addon-essentials'],
+  features: {
+    babelModeV7: true,
+  },
+  babel: (options) => ({
+    ...options,
+    presets: [...options.presets, '@babel/preset-typescript'],
+  }),
 };
