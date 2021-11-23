d8d70a6c3d7d33638765c0ae245ab12b5bf41983
dannyhw <dannyhyunsoowilliams@gmail.com>
2021-11-23 12:51:15 +0000

    fix: more lenient includes

--- preset.js
+++ preset.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const path = require('path');
 
 const getBabelPlugins = (options) => {
   let reactNativeWeb = 'react-native-web';
@@ -8,25 +9,29 @@ const getBabelPlugins = (options) => {
   return [reactNativeWeb];
 };
 
-const getModulesToTranspile = (options) => {
-  if (options.modulesToTranspile && Array.isArray(options.modulesToTranspile)) {
-    return options.modulesToTranspile.map((moduleName) => {
-      // returns the path to the entry point file like addon-react-native-web/node_modules/react-native-reanimated/lib/Animated.js'
-      const fullPath = require.resolve(moduleName);
+const getModule = (name) => path.join('node_modules', name);
 
-      const stringToFind = `node_modules/${moduleName}/`;
+// copied from https://github.com/expo/expo-cli/blob/master/packages/webpack-config/src/loaders/createBabelLoader.ts
+const DEFAULT_INCLUDES = [
+  getModule('react-native'),
+  getModule('react-navigation'),
+  getModule('expo'),
+  getModule('unimodules'),
+  getModule('@react'),
+  getModule('@expo'),
+  getModule('@use-expo'),
+  getModule('@unimodules'),
+  getModule('native-base'),
+  getModule('styled-components'),
+];
 
-      // remove everything after 'node_modules/module_name/'
-      const adjustedPath = fullPath.substring(
-        0,
-        fullPath.indexOf(stringToFind) + stringToFind.length,
-      );
-
-      return adjustedPath;
-    });
-  }
-  return [];
-};
+const DEFAULT_EXCLUDES = [
+  '/node_modules',
+  '/bower_components',
+  '/.expo/',
+  // Prevent transpiling webpack generated files.
+  '(webpack)',
+];
 
 module.exports = {
   babel: async (config, options) => ({
@@ -49,31 +54,51 @@ module.exports = {
       }),
     );
 
-    const modulesToTranspile = getModulesToTranspile(options);
+    const babelPlugins = getBabelPlugins(options);
+    const root = options?.projectRoot ?? process.cwd();
+    const modules = [...DEFAULT_INCLUDES, ...options.modulesToTranspile];
 
-    if (modulesToTranspile.length > 0) {
-      const babelPlugins = getBabelPlugins(options);
+    // fix for uncompiled react-native dependencies
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      loader: 'babel-loader',
+      // include logic copied from https://github.com/expo/expo-cli/blob/master/packages/webpack-config/src/loaders/createBabelLoader.ts
+      include(filename) {
+        if (!filename) {
+          return false;
+        }
 
-      // fix for uncompiled react-native dependencies
-      config.module.rules.push({
-        test: /\.(js|jsx|ts|tsx)$/,
-        loader: 'babel-loader',
-        include: modulesToTranspile,
-        options: {
-          presets: [
-            '@babel/env',
-            [
-              '@babel/preset-react',
-              {
-                runtime: 'automatic',
-              },
-            ],
-            'module:metro-react-native-babel-preset',
+        for (const possibleModule of modules) {
+          if (filename.includes(path.normalize(possibleModule))) {
+            return true;
+          }
+        }
+
+        if (filename.includes(root)) {
+          for (const excluded of DEFAULT_EXCLUDES) {
+            if (filename.includes(path.normalize(excluded))) {
+              return false;
+            }
+          }
+          return true;
+        }
+        return false;
+      },
+      options: {
+        root,
+        presets: [
+          '@babel/env',
+          'module:metro-react-native-babel-preset',
+          [
+            '@babel/preset-react',
+            {
+              runtime: 'automatic',
+            },
           ],
-          plugins: ['@babel/plugin-proposal-class-properties', ...babelPlugins],
-        },
-      });
-    }
+        ],
+        plugins: [...babelPlugins, '@babel/plugin-proposal-class-properties'],
+      },
+    });
 
     config.resolve.extensions = ['.web.js', ...config.resolve.extensions];
 
