b9de11c619c4921f8bc1137492121daa875bc72c
Daniel Williams <daniel.williams@coverflex.com>
2022-04-25 18:42:19 +0100

    chore: update deps

--- stories/libraries/NativeBase/AppBar.tsx
+++ stories/libraries/NativeBase/AppBar.tsx
@@ -8,27 +8,29 @@ import { SearchIcon } from '../Svg/SearchIcon';
 export function AppBar() {
   return (
     <>
-      <StatusBar backgroundColor='#3700B3' barStyle='light-content' />
-      <Box safeAreaTop backgroundColor='#6200ee' />
+      <StatusBar bg='#3700B3' barStyle='light-content' />
+      <Box safeAreaTop bg='#6200ee' />
       <HStack
         bg='#6200ee'
-        px={1}
-        py={3}
+        px='1'
+        py='3'
         justifyContent='space-between'
         alignItems='center'
+        w='100%'
       >
-        <HStack space={4} alignItems='center'>
-          <IconButton icon={<Icon as={MenuIcon} size='sm' color='white' />} />
-          <Text color='white' fontSize={20} fontWeight='bold'>
+        <HStack alignItems='center'>
+          <IconButton icon={<Icon as={MenuIcon} size='lg' color='white' />} />
+
+          <Text color='white' fontSize='20' fontWeight='bold'>
             Home
           </Text>
         </HStack>
         <HStack space={2}>
           <IconButton
-            icon={<Icon as={FavoriteIcon} size='sm' color='white' />}
+            icon={<Icon as={FavoriteIcon} size='lg' color='white' />}
           />
-          <IconButton icon={<Icon as={SearchIcon} color='white' size='sm' />} />
-          <IconButton icon={<Icon as={MoreIcon} size='sm' color='white' />} />
+          <IconButton icon={<Icon as={SearchIcon} color='white' size='lg' />} />
+          <IconButton icon={<Icon as={MoreIcon} size='lg' color='white' />} />
         </HStack>
       </HStack>
     </>
